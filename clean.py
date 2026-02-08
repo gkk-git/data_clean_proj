bc5f272f7bf4d3cf557203094cdcf81777090e05
gkk-git <gowtham.k@skillfloor.com>
2026-02-08 10:26:28 +0530

    Add clean.py

---
+++ clean.py
@@ -0,0 +1,15 @@
+def remove_duplicates(data):
+    """ Removes duplicates in data and return the result"""
+    # TODO
+    return data
+
+
+def remove_null_values(data):
+    """ Removes null values in data and return the result"""
+    # TODO
+    return data
+
+def remove_negative_values(data):
+    """ Remvoes negative values in data and return the result"""
+    # TODO
+    return data
